149a593fd30aee719f387b0e21c64f6d8679525b
gruberinfos <softwareentwickler@gruberinfos.de>
2022-05-09 15:37:16 +0200

    1 file modified

--- array.js
+++ array.js
@@ -1,1 +1,24 @@
+/**
+ * Arrays
+ *
+ * https://www.w3schools.com/jsref/jsref_obj_array.asp
+ *
+ * Arrays in JavaScript sind eher Listen denn:
+ *  - die Länge ist veränderbar
+ *  - die enthaltenden Typen sind unterschiedlich
+ *
+ * Array erstellen
+ *  - leer
+ *  - mit Elementen initialisiert
+ *
+ * Zugriff auf die Elementen
+ *
+ * Ein Array ist ein Objekt. Eigenschaften und Methoden:
+ *  - push(), pop()
+ *  - shift(), unshift()
+ *  - length - die Länge des Array
+ *  - sort - eine sortierung des Array
+ *  - join - Elemente in einen String umwandeln
+ *  - slice -
+ */
 console.log('Ein Array');
